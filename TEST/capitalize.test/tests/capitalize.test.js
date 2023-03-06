859325de27020ba98b49e0c0168788d277d7991d
Nick G <nolimit48@yandex.ru>
2023-03-06 19:38:46 +0300

    add with asserts

--- TEST/capitalize.test/tests/capitalize.test.js
+++ TEST/capitalize.test/tests/capitalize.test.js
@@ -1,12 +1,9 @@
 import { capitalize } from '../src/capitalize.js';
+import { strict as assert } from 'node:assert';
 
-if (capitalize('hello') !== 'Hello') {
-	  throw new Error('Функция работает неверно!');
-}
+assert.equal(capitalize(''), '');
+assert.equal(capitalize('hello'), 'hello');
 
-if (capitalize('') !== '') {
-	  throw new Error('Функция работает неверно!');
-}
 
 console.log('Все тесты пройдены!');
 
